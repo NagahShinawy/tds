121cad0d236b4e5667c7257b6855a3b198306742
nagah <nagah.shinawy@gmail.com>
2024-06-03 12:24:11 +0300

    installation(env): adding .env file

--- studio_management/apps/booking/urls.py
+++ studio_management/apps/booking/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import StudioViewSet, ReservationListView
+from .views import StudioViewSet, ReservationListView, CustomerReservationCancellationView
 
 
 router = DefaultRouter()
